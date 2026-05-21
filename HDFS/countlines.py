6f0587ee0ff0e0dbc633813fef224ec247963bb8
ron <ronjoeagusthy12@gmail.com>
2026-05-21 07:41:17 +0530

    better reducer

--- HDFS/countlines.py
+++ HDFS/countlines.py
@@ -1,15 +1,13 @@
 import sys
-
 for line in sys.stdin:
     if line.strip():
         print("lines", 1)
 
 
 import sys
-
-count = 0
+counts = {}
 for line in sys.stdin:
     key, val = line.strip().split()
-    count += int(val)
-
-print("Total lines:", count)
+    counts[key] = counts.get(key, 0) + int(val)
+for key, value in counts.items():
+    print(key, value)
